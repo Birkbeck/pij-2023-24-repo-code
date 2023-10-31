0c6ba7cddfdfee8e13201a303992e79bb4c17175
Carsten Fuhs <carsten@dcs.bbk.ac.uk>
2023-10-31 07:19:54 +0000

    MeasureSummerRunner gets launch() method

--- worksheets/day6/MeasureSummerRunner.java
+++ worksheets/day6/MeasureSummerRunner.java
@@ -1,9 +1,15 @@
 public class MeasureSummerRunner {
-    public static void main(String[] args) {
+
+    private void launch() {
         Measurable[] ms = new Measurable[2];
         ms[0] = new Point(1,0);
         ms[1] = new Horse(20); //Point(3, 4);
         double d = MeasureSummer.sumMeasures(ms);
         System.out.println(d);
+    }
+
+    public static void main(String[] args) {
+        MeasureSummerRunner runner = new MeasureSummerRunner();
+        runner.launch();
     }
 }
