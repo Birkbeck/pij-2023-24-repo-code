8a85587ef52486e0d0a8af04c7e0a1fd8b6bc1dd
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2023-11-01 15:26:29 +0000

    Day 10 code for Ex 3

---
+++ worksheets/day10/pij/day10/Day10Exercise3.java
@@ -0,0 +1,34 @@
+package pij.day10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day10Exercise3 {
+    public void launch(int userInput) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        intList.add(6);
+        try {
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+    }
+}
